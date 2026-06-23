afe01770032dd8982eb72519c320aac83f93c42e
Yash Jain <33027863+yashjain240698@users.noreply.github.com>
2026-06-23 12:23:47 +0530

    Add: car-fleet - submission-1

---
+++ Data Structures & Algorithms/car-fleet/submission-1.java
@@ -0,0 +1,32 @@
+class Solution {
+    class Node{
+        int idx;
+        int pos;
+        int speed;
+        Node(int idx, int pos, int speed){
+            this.idx = idx;
+            this.pos = pos;
+            this.speed = speed;
+        }
+    }
+    Deque<Double> st = new ArrayDeque<>();
+    public int carFleet(int target, int[] position, int[] speed) {
+        PriorityQueue<Node> pq = new PriorityQueue<>((a,b) -> a.pos - b.pos);
+        int l = position.length;
+        for(int i = 0 ; i < l ; i++){
+            pq.offer(new Node(i, position[i],speed[i]));
+        }
+        Node tmp;Double time;
+        while(!pq.isEmpty()){
+            tmp = pq.poll();
+            time = (target - tmp.pos)/(double) tmp.speed;
+            //System.out.println(time);
+            while(!st.isEmpty() && (st.peek() <= time)){
+                st.pop();
+            }
+            st.push(time);
+        }
+        return st.size();
+    }
+
+}
