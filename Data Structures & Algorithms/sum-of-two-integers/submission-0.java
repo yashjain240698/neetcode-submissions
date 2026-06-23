56be940e25bab545e2621de1aa6e400d5ad44fc0
Yash Jain <33027863+yashjain240698@users.noreply.github.com>
2026-06-24 00:17:07 +0530

    Add: sum-of-two-integers - submission-0

---
+++ Data Structures & Algorithms/sum-of-two-integers/submission-0.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int getSum(int a, int b) {
+        int ans = 0, carry = 0;
+        for(int i = 0 ; i < 32 ; i++){
+            if(((a&(1<<i)) != 0) && ((b&(1<<i)) != 0)){
+                if(carry == 1){
+                    ans = ans | 1<<i;
+                    carry = 1 ;
+                } else {
+                    carry = 1 ;
+                }
+            } else if(((a&(1<<i)) != 0) || ((b&(1<<i)) != 0)){
+                if(carry == 0){
+                    ans = ans | 1<<i;
+                } else {
+                    carry = 1;
+                }
+            } else {
+                if(carry == 1){
+                    ans = ans | 1<<i;
+                    carry = 0;
+                }
+            }
+        }
+        return ans;
+    }
+}
