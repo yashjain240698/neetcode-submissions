f0188af607c0d6a1079050ea3e4405d86a8865c8
Yash Jain <33027863+yashjain240698@users.noreply.github.com>
2026-07-08 23:19:22 +0530

    Add: subsets-ii - submission-4

---
+++ Data Structures & Algorithms/subsets-ii/submission-4.java
@@ -0,0 +1,22 @@
+class Solution {
+    List<List<Integer>> s = new ArrayList<>();
+    private void helper(int[] nums, int idx, List<Integer> l){
+        if(idx >= nums.length){
+            s.add(new ArrayList(l));
+            return;
+        }
+        int j = idx;
+        while((j < nums.length) && (nums[j] == nums[idx])){
+            j++;
+        }
+        helper(nums, j, l);
+        l.add(nums[idx]);
+        helper(nums, idx+1, l);
+        l.remove(l.size()-1);
+    }
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+        helper(nums, 0, new ArrayList<>());
+        return s;
+    }
+}
